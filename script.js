a348c3b2644c52fea89e696c345829bdbbc149a5
rajyadav <vector36razz@gmail.com>
2025-02-04 22:24:06 +0530

    finished

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function toggleMobileMenu(){
     document.getElementById('menu').classList.toggle("active");
 }
+
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Check user preference from localStorage
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  toggleBtn.textContent = "☀️ Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
